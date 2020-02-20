e732b4a463be5bc06fb9e00080a299e0da87cf68
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-20 17:18:06 +0900

    画像アップロード機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.includes(:images).order('created_at DESC')
   end
 
   def new
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +28,10 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :status, :description, :charge, :area, :day, :sale, images_attributes: [:picture])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   # belongs_to brand, foreign_key: 'brand_id'
   # belongs_to category
   has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
